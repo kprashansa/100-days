f09d3b8b7c16c7328a1d34073dab2caca217264e
KUMARI PRASHANSA <kumariprashansa@KUMARIs-MacBook-Air.local>
2026-09-14 00:55:47 +0530

    Day 14

---
+++ 14.1.c
@@ -0,0 +1,19 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    long long product = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product = product * i;
+    }
+
+    printf("Product of even numbers from 1 to %d = %lld\n", n, product);
+
+    return 0;
+}

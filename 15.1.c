ac77fe550208ac6d455a2e8308e2c147f65f0a9e
KUMARI PRASHANSA <kumariprashansa@KUMARIs-MacBook-Air.local>
2026-09-14 00:55:47 +0530

    Day 15

---
+++ 15.1.c
@@ -0,0 +1,22 @@
+//Q29: Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial of a negative number is not defined.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        printf("Factorial of %d = %lld\n", n, factorial);
+    }
+
+    return 0;
+}

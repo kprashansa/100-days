277ce64620aeff3b2d1f26323672f35b7a18e306
KUMARI PRASHANSA <kumariprashansa@KUMARIs-MacBook-Air.local>
2026-09-14 00:55:47 +0530

    Day 31

---
+++ 31.1.c
@@ -0,0 +1,38 @@
+// Q61: Search for an element in an array using linear search.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, num;
+    int found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &num);
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == num)
+        {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}

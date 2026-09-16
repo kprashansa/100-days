d26baf1d40c6797ee11f00fc7034894ff65bab4b
kprashansa <kprashansamahto06@gmail.com>
2026-09-16 23:38:36 +0530

    Add files via upload

---
+++ 38.1.c
@@ -0,0 +1,53 @@
+//Q75: Add two matrices.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+2 2
+5 6
+7 8
+Output 1:
+6 8
+10 12
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int rows, cols, i, j;
+
+    scanf("%d %d", &rows, &cols);
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    scanf("%d %d", &rows, &cols);
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ 38.2.c
@@ -0,0 +1,49 @@
+//Q76: Check if a matrix is symmetric.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+2 1
+Output 1:
+True
+
+Input 2:
+2 2
+1 0
+2 1
+Output 2:
+False
+
+*/
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10];
+    int n, i, j, symmetric = 1;
+
+    scanf("%d %d", &n, &n);
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}

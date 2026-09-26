4ffea9919afac8ca06ee26851b3be89b710574bd
kprashansa <kprashansamahto06@gmail.com>
2026-09-26 22:03:53 +0530

    Add files via upload

---
+++ 48.2.c
@@ -0,0 +1,45 @@
+//Q96: Reverse each word in a sentence without changing the word order.
+
+/*
+Sample Test Cases:
+Input 1:
+I love coding
+Output 1:
+I evol gnidoc
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+    int i, start = 0, end;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] == ' ' || str[i] == '\n')
+        {
+            end = i - 1;
+
+            while(start < end)
+            {
+                char temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+
+                start++;
+                end--;
+            }
+
+            start = i + 1;
+        }
+    }
+
+    printf("Output: %s", str);
+
+    return 0;
+}
